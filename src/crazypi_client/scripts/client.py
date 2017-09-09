3f82f9bc0f3eb67c29fa9927ed3563309c49c6b0
Wenhua Shi <wenhua.shi@tum.de>
2017-09-09 18:23:43 +0200

    add layout

--- src/crazypi_client/scripts/client.py
+++ src/crazypi_client/scripts/client.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import sys
 import os
+
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import *
 
 import rospy
 from geometry_msgs.msg import Twist
 from time import sleep
+
+def h1():
+    os.environ["ROS_MASTER_URI"] = "http://192.168.99.1:11311"
+
+    print "sending message..."
+    try:
+        go1()
+    except rospy.ROSInterruptException:
+        pass
+
+
 
 
 def go1():
@@ -11,7 +28,7 @@ def go1():
     print "node initialized"
 
     pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
-    
+
     twist = Twist()
     twist.linear.x = 2.0
     twist.linear.y = 2.0
@@ -20,18 +37,75 @@ def go1():
     twist.angular.y = 0.0
     twist.angular.z = 0.0
 
-    for i in range(0, 1):
+    for i in range(0, 10):
         pub.publish(twist)
         sleep(0.05)
 
-if __name__ == '__main__':
-    os.environ["ROS_MASTER_URI"] = "http://192.168.18.16:11311"
-    os.environ["ROS_IP"] = "192.168.18.78"
 
-    print "sending message..."
-    try:
-        go1()
-    except rospy.ROSInterruptException:
-        pass
+class MainWindow(QtGui.QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(QtGui.QLabel('SB_GZX'))
+        self.setLayout(layout)
+        print 'ok'
+
+
+
+class ConfigWindow(QtGui.QWidget):
+    def __init__(self):
+        super(ConfigWindow, self).__init__()
+
+        layout = QtGui.QVBoxLayout()
+
+        h0 = QtGui.QHBoxLayout()
+        l0 = QtGui.QLabel('Local IP: ')
+        l0.setStyleSheet("font-family: Monospace;")
+        self.t0 = t0 = QtGui.QLineEdit()
+        h0.addWidget(l0)
+        h0.addWidget(t0)
+        layout.addLayout(h0)
+
+        h1 = QtGui.QHBoxLayout()
+        l1 = QtGui.QLabel('Robot IP: ')
+        l1.setStyleSheet("font-family: Monospace;")
+        self.t1 = t1 = QtGui.QLineEdit()
+        h1.addWidget(l1)
+        h1.addWidget(t1)
+        layout.addLayout(h1)
+
+
+        button = QtGui.QPushButton('Connect')
+        layout.addWidget(button)
+        button.clicked.connect(self.connect)
+
+        self.setLayout(layout)
+
+
+    def connect(self):
+        os.environ["ROS_MASTER_URI"] = "http://" + str(self.t1.text()) + ":11311"
+        os.environ["ROS_IP"] = str(self.t0.text())
+        print os.environ["ROS_MASTER_URI"]
+        print os.environ["ROS_IP"]
+
+        self.mainWindow = MainWindow()
+        self.mainWindow.show()
+        self.close()
+
+
+
+
+if __name__ == '__main__':
+
+    # Create an PyQT4 application object.
+    app = QApplication(sys.argv)
+
+    # The QWidget widget is the base class of all user interface objects in PyQt4.
+    configWindow = ConfigWindow()
+
+    # Show window
+    configWindow.show()
+
+    sys.exit(app.exec_())
 
 
